f8b4ce4c59970537148048346c0555c9bd24321e
vcokltfre <vcokltfre@gmail.com>
2020-09-20 11:08:22 +0100

    feat: add ping command

--- bot/cogs/utility/general.py
+++ bot/cogs/utility/general.py
@@ -1,3 +1,4 @@
+import time
 import discord
 from discord.ext import commands
 
@@ -76,6 +77,15 @@ class General(commands.Cog):
         await ctx.channel.send(f"Restarting {name}")
         self.bot.logger.info(f"Shutting down {name}")
         await self.bot.close()
+
+    @commands.command(name="ping")
+    @commands.has_any_role("Administrator", "Moderator", "Big Brain")
+    async def ping(self, ctx: commands.Context):
+        t_start = time.time()
+        m = await ctx.channel.send("Testing RTT for message editing.")
+        await m.edit(content="Testing...")
+        rtt = time.time() - t_start
+        await m.edit(content=f"Pong!\nMessage edit RTT: {round(rtt*1000, 2)}ms\nWebsocket Latency: {round(self.bot.latency*1000, 2)}ms")
 
 def setup(bot: Bot):
     bot.add_cog(General(bot))
